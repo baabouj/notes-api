9013c453f62650e0d49a43e4b0e6c559bd00d6bb
Omar Baabouj <baabouj.dev@gmail.com>
2023-12-31 11:18:40 +0100

    docs: add and configure swagger

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { authRouter } from './auth.route';
 import { categoriesRouter } from './categories.route';
+import { docsRouter } from './docs.route';
 import { notesRouter } from './notes.route';
 import { tagsRouter } from './tags.route';
 
@@ -11,5 +12,6 @@ v1Router.use('/auth', authRouter);
 v1Router.use('/notes', notesRouter);
 v1Router.use('/categories', categoriesRouter);
 v1Router.use('/tags', tagsRouter);
+v1Router.use('/docs', docsRouter);
 
 export { v1Router };
