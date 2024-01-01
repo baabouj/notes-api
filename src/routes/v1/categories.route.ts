d31ac7e6dc1d6edb457d7cf8180cdcbf4a23ddb9
Omar Baabouj <baabouj.dev@gmail.com>
2024-01-01 12:20:10 +0100

    fix: add api documentation

--- src/routes/v1/categories.route.ts
+++ src/routes/v1/categories.route.ts
@@ -21,3 +21,255 @@ categoriesRouter.get(
 );
 
 export { categoriesRouter };
+
+/**
+ * @swagger
+ * tags:
+ *   name: Categories
+ *   description: Category management
+ */
+
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     summary: Create a category
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *             example:
+ *               name: work
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Category'
+ *       400:
+ *         $ref: '#/components/responses/ValidationFailed'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *
+ *   get:
+ *     summary: Get all categories
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         default: 10
+ *         description: Maximum number of categories
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: Page number
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *         description: search for categories
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 info:
+ *                   type: object
+ *                   properties:
+ *                     total:
+ *                       type: integer
+ *                       example: 100
+ *                     current_page:
+ *                       type: integer
+ *                       example: 2
+ *                     next_page:
+ *                       type: integer
+ *                       example: 3
+ *                     prev_page:
+ *                       type: integer
+ *                       example: 1
+ *                     last_page:
+ *                       type: integer
+ *                       example: 4
+ *                     per_page:
+ *                       type: integer
+ *                       example: 20
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     allOf:
+ *                        - $ref: '#/components/schemas/Category'
+ *                        - type: object
+ *                          properties:
+ *                            count:
+ *                              type: integer
+ *                              description: number of notes in category
+ *                              example: 5
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       400:
+ *         $ref: '#/components/responses/ValidationFailed'
+ */
+
+/**
+ * @swagger
+ * /categories/{categoryId}:
+ *   get:
+ *     summary: Get a category
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: categoryId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Category id
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                  - $ref: '#/components/schemas/Category'
+ *                  - type: object
+ *                    properties:
+ *                      count:
+ *                        type: integer
+ *                        description: number of notes in category
+ *                        example: 5
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   patch:
+ *     summary: Update a category
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: categoryId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Category id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: work
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Category'
+ *       400:
+ *         $ref: '#/components/responses/ValidationFailed'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   delete:
+ *     summary: Delete a category
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: categoryId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Category id
+ *     responses:
+ *       204:
+ *         description: No Content
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ */
+
+/**
+ * @swagger
+ * /categories/{categoryId}/notes:
+ *   get:
+ *     summary: Get category's notes
+ *     tags: [Categories]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: categoryId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Category id
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         default: 10
+ *         description: Maximum number of notes
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: Page number
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *         description: search for notes
+ *       - in: query
+ *         name: include
+ *         schema:
+ *           type: string
+ *           enum:
+ *             - tags
+ *         description: include notes tags
+ *     responses:
+ *       200:
+ *         $ref: '#/components/responses/PaginatedNotes'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ */
--- src/routes/v1/notes.route.ts
+++ src/routes/v1/notes.route.ts
@@ -16,3 +16,225 @@ notesRouter.get('/:noteId/category', noteController.findNoteCategory);
 notesRouter.get('/:noteId/tags', noteController.findNoteTags);
 
 export { notesRouter };
+
+/**
+ * @swagger
+ * tags:
+ *   name: Notes
+ *   description: Note management
+ */
+
+/**
+ * @swagger
+ * /notes:
+ *   post:
+ *     summary: Create a note
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *        $ref: '#/components/requestBody/Note'
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Note'
+ *       400:
+ *         $ref: '#/components/responses/ValidationFailed'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *
+ *   get:
+ *     summary: Get all notes
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         default: 10
+ *         description: Maximum number of notes
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: Page number
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *         description: search for notes
+ *       - in: query
+ *         name: include
+ *         schema:
+ *           type: string
+ *           enum:
+ *             - category
+ *             - tags
+ *             - tags,category
+ *             - category,tags
+ *         description: include notes tags or categories or both
+ *     responses:
+ *       200:
+ *         $ref: '#/components/responses/PaginatedNotes'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       400:
+ *         $ref: '#/components/responses/ValidationFailed'
+ */
+
+/**
+ * @swagger
+ * /notes/{noteId}:
+ *   get:
+ *     summary: Get a note
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: noteId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Note id
+ *       - in: query
+ *         name: include
+ *         schema:
+ *           type: string
+ *           enum:
+ *             - category
+ *             - tags
+ *             - tags,category
+ *             - category,tags
+ *         description: include notes tags or categories or both
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Note'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   patch:
+ *     summary: Update a note
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: noteId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Note id
+ *     requestBody:
+ *        $ref: '#/components/requestBody/Note'
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Note'
+ *       400:
+ *         $ref: '#/components/responses/ValidationFailed'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   delete:
+ *     summary: Delete a note
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: noteId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Note id
+ *     responses:
+ *       204:
+ *         description: No Content
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ */
+
+/**
+ * @swagger
+ * /notes/{noteId}/category:
+ *   get:
+ *     summary: Get note's category
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: noteId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Note id
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                allOf:
+ *                  - $ref: '#/components/schemas/Category'
+ *                  - type: object
+ *                    properties:
+ *                      count:
+ *                        type: integer
+ *                        description: number of notes in category
+ *                        example: 5
+ *       204:
+ *         description: No Content
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ */
+
+/**
+ * @swagger
+ * /notes/{noteId}/tags:
+ *   get:
+ *     summary: Get note's tags
+ *     tags: [Notes]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: noteId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Note id
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Tag'
+ *       401:
+ *         $ref: '#/components/responses/Unauthorized'
+ */
